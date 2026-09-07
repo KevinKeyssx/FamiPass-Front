7a505e32ca16508c5de1e0cbc89d5e5bd6c409e4
kevinkeyssx <keyss1700@gmail.com>
2026-09-06 22:58:40 -0300

    LayoutServerLoad updated

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,28 @@
-import type { LayoutServerLoad } from './$types.js';
+import type { LayoutServerLoad }       from './$types.js';
+import { getUserByEmail, getUserRole } from '$lib/server/supabase/services/users.service.js';
+import type { UserRole }               from '$lib/types/index.js';
 
 export const load: LayoutServerLoad = async ( { locals } ) => {
+	let user = null;
+	let role : UserRole = 'MEMBER';
+
+	if ( locals.user && locals.user.email ) {
+		role = ( await getUserRole( locals.user.email )) || ( 'MEMBER' as UserRole );
+
+        const dbUser = await getUserByEmail( locals.user.email );
+
+		user = {
+			id        : locals.user.id,
+			name      : locals.user.name ?? dbUser?.user_name ?? locals.user.email.split( '@' )[ 0 ],
+			user_name : dbUser?.user_name ?? locals.user.name,
+			email     : locals.user.email,
+			image     : ( locals.user as any ).image ?? null
+		};
+	}
+
 	return {
-		user    : locals.user,
+		user,
+		role,
 		session : locals.session
 	};
 };
