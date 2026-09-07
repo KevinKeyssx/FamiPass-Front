34c955678b5ede79816ebddad8176bb4e803fb5e
kevinkeyssx <keyss1700@gmail.com>
2026-09-06 22:59:17 -0300

    LayoutServerLoad updated

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,17 +1,29 @@
-import { redirect }        from '@sveltejs/kit';
-import type { LayoutServerLoad } from './$types.js';
-import { getUserRole }         from '$lib/server/supabase/services/users.service.js';
-import type { UserRole }        from '$lib/types/index.js';
+import { redirect } from '@sveltejs/kit';
+
+import {
+    getUserByEmail,
+    getUserRole
+}                                   from '$lib/server/supabase/services/users.service.js';
+import type { LayoutServerLoad }    from './$types.js';
+import type { UserRole }            from '$lib/types/index.js';
+
 
 export const load: LayoutServerLoad = async ( { locals } ) => {
 	if ( !locals.user || !locals.user.email ) {
 		throw redirect( 302, '/login' );
 	}
 
-	const role = ( await getUserRole( locals.user.email ) ) || ( 'MEMBER' as UserRole );
+	const role   = ( await getUserRole( locals.user.email ) ) || ( 'MEMBER' as UserRole );
+	const dbUser = await getUserByEmail( locals.user.email );
 
 	return {
-		user : locals.user,
+		user : {
+			id        : locals.user.id,
+			name      : locals.user.name ?? dbUser?.user_name ?? locals.user.email.split( '@' )[ 0 ],
+			user_name : dbUser?.user_name ?? locals.user.name,
+			email     : locals.user.email,
+			image     : ( locals.user as any ).image ?? null
+		},
 		role : role
 	};
 };
