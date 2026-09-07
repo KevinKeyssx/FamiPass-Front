f545e3972e0f6cc22ce33e0a98852fa43812edba
kevinkeyssx <keyss1700@gmail.com>
2026-09-06 22:58:08 -0300

    familyMember return updated

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -3,4 +3,4 @@ export type { UserRole, User }                                          from './
 export type { Family, FamilyEvent }                                     from './family.js';
 export type { Product, EventProduct, EventProductStatus }               from './product.js';
 export type { OrderStatus, FamilyMemberSnapshot, OrderItem, Order }     from './order.js';
-export type { CommunityOrganization, FamilyMember }                     from './familyMember.js';
+export type { CommunityOrganization, FamilyMemberRole, FamilyMember }   from './familyMember.js';
